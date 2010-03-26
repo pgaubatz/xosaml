e0a81675ed6598784e0438c54fabaa15d09a219a
pgaubatz <pgaubatz@localhost>
2010-03-26 12:49:29 +0100

    Added support for <any> and <anyAttribute> to the xoXSD framework. Added the example Any-and-AnyAttribute.tcl.

---
+++ examples/Any-and-AnyAttribute.tcl
@@ -0,0 +1,36 @@
+#!/usr/bin/tclsh
+
+set auto_path [linsert $auto_path 0 ../packages/xoXSD/]
+source SAML-Core.xotcl
+
+#
+#<element name="SubjectConfirmationData" type="saml:SubjectConfirmationDataType"/>
+#<complexType name="SubjectConfirmationDataType" mixed="true">
+#	<complexContent>
+#		<restriction base="anyType">
+#			<sequence>
+#				<any namespace="##any" processContents="lax" minOccurs="0" maxOccurs="unbounded"/>
+#			</sequence>
+#			<attribute name="NotBefore" type="dateTime" use="optional"/>
+#			<attribute name="NotOnOrAfter" type="dateTime" use="optional"/>
+#			<attribute name="Recipient" type="anyURI" use="optional"/>
+#			<attribute name="InResponseTo" type="NCName" use="optional"/>
+#			<attribute name="Address" type="string" use="optional"/>
+#			<anyAttribute namespace="##other" processContents="lax"/>
+#		</restriction>
+#	</complexContent>
+#</complexType>
+#
+
+Audience aud1 "http://audience"
+
+SubjectConfirmationData s1
+
+s1 printSlots
+
+s1 Address "http://address"
+s1 addAny aud1
+s1 addAnyAttribute ANewAttribute ::xoXSD::DataTypes::string "SomeContent"
+s1 print
+
+s1 printSlots
